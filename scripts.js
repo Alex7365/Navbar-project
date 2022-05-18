fb1f8c8604ccfde2ada68a3a61848b3b2a8ab625
Alex <espia816@hsd.k12.or.us>
2022-05-18 14:53:20 -0700

    change id, add transition, and js functions

--- scripts.js
+++ scripts.js
@@ -1,28 +1,33 @@
+var mobileBreakpoint = 690;
 
 // when the page loads, add an open class to nav if the screen is wide enough. //
 window.onload = function() {
     var screenWidth = window.innerWidth;
-    var mobileBreakpoint = 690;
     var globalNavbar = document.getElementById("global-nav");
     var toggle = document.getElementById("menu-toggle");
 
-    // add 'open' class to the ul in the navbar if wider than // the breakpoint
+    // add 'open' class to the ul in the navbar if wider than the breakpoint
     if (screenWidth > mobileBreakpoint) {
         globalNavbar.classList.add("open");
-        toggle = document.classList.add("open");
+        toggle.classList.add("open");
+    }
+    // add a toggle click function, which serves as an on-off button
+    toggle.onclick = function() {
+        globalNavbar.classList.toggle("open");
+        toggle.classList.toggle("open");
     }
 }
 window.onresize = function() {
     var screenWidth = window.innerWidth;
-    var mobileBreakpoint = 690;
-    var globalNavbar = document.getElementById("global-nav")
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
 
     if (screenWidth > mobileBreakpoint) {
         globalNavbar.classList.add("open");
-        toggle = document.classList.add("open");
+        toggle.classList.add("open");
     }
     else {
         globalNavbar.classList.remove("open");
-        toggle = document.classList.remove("open");
+        toggle.classList.remove("open");
     }
 }
